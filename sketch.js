563c5c2d06f28c58d9497d3baf633e39e8d97f31
Afifa-max <72868076+Afifa-max@users.noreply.github.com>
2020-10-23 10:09:01 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var car
+var wall,thickness;
+var bullet,speed,weight;
+
+
+function setup() {
+  createCanvas(1600,400);
+  
+
+  car=createSprite(50,200,50,50);
+  car.shapeColor='cyan';
+
+  wall=createSprite(1200,200,thickness,height/2);
+
+  car.velocityX=speed;
+
+  thickness=random(22,83)
+
+  speed=random(223,321)
+
+  weight=random(50,32)
+
+}
+
+function draw() {
+  background(255,255,255);
+  
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    var damage= 0.5 * weight * speed * speed/(thickness*thickness*thickness);
+
+if (damage>10){
+
+wall.shapeColor= color (255,0,0);
+
+}
+
+if (damage<10){
+
+  wall.shapeColor= color (0,255,0);
+  
+  }
+
+
+  }
+
+  drawSprites();
+}
+
+function hasCollided(lbullet,lwall){
+
+bulletRightEdge=lbullet.x + lbullet.width;
+wallLeftEdge=lwall.x;
+
+if (bulletRightEdge>=wallLeftEdge){
+  return true
+}
+
+return false;
+
+
+}
